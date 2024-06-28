91c13ecc77252dd4cbeef4b0f52f178adda0bdaa
Herlan <herlanherbas@gmail.com>
2024-06-27 20:59:00 -0400

    create page userList

---
+++ src/pages/UserList.jsx
@@ -0,0 +1,36 @@
+import useFetchGet from "../hooks/useFetchGet";
+
+export default function UserListt() {
+  const {
+    data: users,
+    loading: loadingUsers,
+    error: errorUsers,
+  } = useFetchGet("users");
+  const {
+    data: albums,
+    loading: loadingAlbums,
+    error: errorAlbums,
+  } = useFetchGet("albums");
+
+  return (
+    <>
+      <h2>Fectch Users</h2>
+      {loadingUsers ? (
+        <p>Loading users...</p>
+      ) : (
+        <ol>
+          {users.map((user) => {
+            return <li key={user.id}>{user.name}</li>;
+          })}
+        </ol>
+      )}
+      <h2>Albums</h2>
+      <button>Get albums</button>
+      <ol>
+        {albums.map((album) => {
+          return <li key={album.id}>{album.title}</li>;
+        })}
+      </ol>
+    </>
+  );
+}
