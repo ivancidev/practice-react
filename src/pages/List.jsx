4779f14e730699499447764c2a29d2bff6724871
Herlan <herlanherbas@gmail.com>
2024-06-27 20:59:32 -0400

    create List page

---
+++ src/pages/List.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import tasksValues from "../utils/tasks";
+import { GoTasklist } from "react-icons/go";
+import AddTask from "../components/AddTask";
+
+const Item = ({ name, done }) => {
+  return <li>{name}{done? "✅" : "❌"}</li>;
+};
+
+export const List = () => {
+  const [tasks, setTasks] = useState(tasksValues);
+  console.log(tasks);
+  return (
+    <>
+      <h1>List taks <GoTasklist style={{ fontSize: "1em" }} /></h1>
+      <AddTask setTasks={setTasks} />
+      <ol>
+        {tasks.map((task) => {
+          return <Item key={task.name} name={task.name} done={task.done}></Item>;
+        })}
+      </ol>
+    </>
+  );
+};
