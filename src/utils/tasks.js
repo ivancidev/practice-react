8dc9c1165126188a1c4c89f71924d9314f627e30
Herlan <herlanherbas@gmail.com>
2024-06-27 20:59:58 -0400

    create tasks

---
+++ src/utils/tasks.js
@@ -0,0 +1,11 @@
+let tasksValues = [
+    { name: "what is React", done: true },
+    { name: "what is React Native", done: false },
+    { name: "what is ReactJS", done: true },
+    { name: "what is Redux", done: false },
+    { name: "what is React Router", done: false },
+    { name: "what is React Hooks", done: true },
+    { name: "what is React Context", done: false },
+  ];
+
+  export default tasksValues;
