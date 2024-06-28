7186593ce8aa04387b4266a0d788d3b88080c939
Herlan <herlanherbas@gmail.com>
2024-06-27 20:57:43 -0400

    init react vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { List } from './pages/List'
+import UserListt from './pages/UserList'
+
+function App() {
+  return (
+    <>
+      {/* <List /> */}
+      <UserListt />
+    </>
+  )
+}
+
+export default App
