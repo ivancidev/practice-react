ccab90b61d057ba044c60eb02805503828b065ab
Herlan <herlanherbas@gmail.com>
2024-07-03 22:11:06 -0400

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import "./App.css";
 import Counter from "./components/Counter";
-import { ListTasks } from "./pages/ListTasks";
-import UserList from "./pages/UserList";
+import { ListTasks } from "./pages/List/ListTasks";
+import Register from "./pages/Register/Register";
+import UserList from "./pages/List/UserList";
+import ShowCard from "./pages/ShowCard/ShowCard";
+import Calculator from "./pages/CalculatorMemo/Calculator";
+import Callback from "./components/Callback/Callback";
+import CounterReducer from "./components/Reducers/CounterReducer";
+import TodoList from "./components/Reducers/TodoList";
 
 function App() {
   return (
     <>
       {/* <ListTasks /> */}
       {/* <UserList /> */}
-      <Counter />
+      {/* <Counter /> */}
+      {/* <Register /> */}
+      <ShowCard />
+      {/* <Calculator /> */}
+      {/* <Callback /> */}
+      {/* Contador con useReducer */}
+      {/* <CounterReducer /> */}
+      {/* <TodoList /> */}
     </>
   );
 }
